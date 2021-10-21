6b197e4de666c323256a159e1bf92943b235f0fe
yeqing <yeqing.wwh@antgroup.com>
2021-10-21 17:20:47 +0800

    Initial commit

---
+++ src/main/java/com/example/mybase/MyBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mybase/MyBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mybase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
